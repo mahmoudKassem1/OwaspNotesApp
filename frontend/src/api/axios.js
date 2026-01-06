33bb2ace11d5dcbc20a3c439df1bd9257e1039c6
mahmoudKassem1 <mahmoudmkassem50@gmail.com>
2026-01-06 14:03:04 +0200

    Fix Axios base URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
+
+const API_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : 'http://localhost:5000/api';          
+
 export default axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_URL,
   withCredentials: true,
 });
